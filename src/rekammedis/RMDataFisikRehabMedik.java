e3e5bdaf708f976a9f307198c48a47738a03453c
khorqoa <khorqoazzahro@gmail.com>
2025-01-07 11:00:28 +0700

    fix edit rehab medik

--- src/rekammedis/RMDataFisikRehabMedik.java
+++ src/rekammedis/RMDataFisikRehabMedik.java
@@ -2412,7 +2412,7 @@ public final class RMDataFisikRehabMedik extends javax.swing.JDialog {
     }
 
     private void ganti() {
-        if(Sequel.mengedittf("fisik_rehab_medik","no_rawat=?","no_rawat=?,kd_dokter=?,anamesa=?,fisik_ujifungsi=?,pemeriksaan_penunjang=?,hasil_laborat=?,diagnosa_utama=?,kd_diagnosa_utama=?,diagnosa_sekunder=?,kd_diagnosa_sekunder=?,diagnosa_sekunder2=?,kd_diagnosa_sekunder2=?,diagnosa_sekunder3=?,kd_diagnosa_sekunder3=?,diagnosa_sekunder4=?,kd_diagnosa_sekunder4=?,prosedur_utama=?,kd_prosedur_utama=?,prosedur_sekunder=?,kd_prosedur_sekunder=?,prosedur_sekunder2=?,kd_prosedur_sekunder2=?,prosedur_sekunder3=?,kd_prosedur_sekunder3=?,kondisi_pulang=?,obat_pulang=?",27,new String[]{
+        if(Sequel.mengedittf("fisik_rehab_medik","no_rawat=?","no_rawat=?,kd_dokter=?,anamesa=?,fisik_ujifungsi=?,pemeriksaan_penunjang=?,diagnosa_utama=?,kd_diagnosa_utama=?,diagnosa_sekunder=?,kd_diagnosa_sekunder=?,diagnosa_sekunder2=?,kd_diagnosa_sekunder2=?,diagnosa_sekunder3=?,kd_diagnosa_sekunder3=?,diagnosa_sekunder4=?,kd_diagnosa_sekunder4=?,prosedur_utama=?,kd_prosedur_utama=?,prosedur_sekunder=?,kd_prosedur_sekunder=?,prosedur_sekunder2=?,kd_prosedur_sekunder2=?,prosedur_sekunder3=?,kd_prosedur_sekunder3=?,anjuran=?,evaluasi=?,suspek=?",27,new String[]{
                 TNoRw.getText(),KodeDokter.getText(),anamesa.getText(),fisikujifungsi.getText(),PemeriksaanPenunjang.getText(), 
                 DiagnosaUtama.getText(),KodeDiagnosaUtama.getText(),DiagnosaSekunder1.getText(),KodeDiagnosaSekunder1.getText(),DiagnosaSekunder2.getText(), 
                 KodeDiagnosaSekunder2.getText(),DiagnosaSekunder3.getText(),KodeDiagnosaSekunder3.getText(),DiagnosaSekunder4.getText(),KodeDiagnosaSekunder4.getText(), 
